d8f282a7e7e7926da05654cf318c1db722c89d5f
Nicolás de Prat Gay <nicoprat@hotmail.com>
2022-06-16 17:58:06 -0300

    Un par de resoluciones antes del fds

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -1,6 +1,12 @@
 // No cambies los nombres de las funciones.
 
 function obtenerMayor(x, y) {
+  if (x > y){
+    return x
+  }
+  else {
+    return y
+  }
   // "x" e "y" son números enteros (int).
   // Devuelve el número más grande
   // Si son iguales, devuelve cualquiera de los dos
@@ -8,12 +14,27 @@ function obtenerMayor(x, y) {
 }
 
 function mayoriaDeEdad(edad) {
+  if (edad >= 18) {
+    return 'Allowed'
+} 
+  else {
+    return 'Not allowed'
+  }
   //Determinar si la persona según su edad puede ingresar a un evento.
   //Si tiene 18 años ó más, devolver --> "Allowed"
   //Si es menor, devolver --> "Not allowed"
 }
   
 function conection(status) {
+  if (status === 1) {
+    return 'Online'
+  }
+  else if (status === 2) {
+    return 'Away'
+  }
+  else {
+    return 'Offline'
+  }
   //Recibimos un estado de conexión de un usuario representado por un valor numérico. 
   //Cuando el estado es igual a 1, el usuario está "Online"
   //Cuando el estado es igual a 2, el usuario está "Away"
@@ -22,6 +43,18 @@ function conection(status) {
 }
 
 function saludo(idioma) {
+  if (idioma === 'aleman') {
+    return 'Guten Tag!'
+  }
+  else if (idioma === 'mandarin') {
+    return 'Ni Hao!'
+  }
+  else if (idioma === 'ingles') {
+    return 'Hello!'
+  }
+  else {
+    return "Hola!"
+  }
   // Devuelve un saludo en tres diferentes lenguajes:
   // Si "idioma" es "aleman", devuelve "Guten Tag!"
   // Si "idioma" es "mandarin", devuelve "Ni Hao!"
@@ -31,6 +64,24 @@ function saludo(idioma) {
 }
 
 function colors(color) {
+  switch (color) {
+    case 'blue': {
+      return 'This is blue';
+    }
+    case 'red': {
+      return 'This is red';
+    }
+    case 'green': {
+      return 'This is green';
+    }
+    
+    case 'orange': {
+      return 'This is orange';
+    }
+    default:
+      return 'Color not found'
+    
+  }
   //La función recibe un color. Devolver el string correspondiente:
   //En caso que el color recibido sea "blue", devuleve --> "This is blue"
   //En caso que el color recibido sea "red", devuleve --> "This is red"
@@ -41,6 +92,13 @@ function colors(color) {
 }
 
 function esDiezOCinco(numero) {
+  if (numero === 5 || numero === 10){
+  return true
+  }
+  else {
+    return false
+  }
+
   // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
   // Tu código:
