8688c3a1d0dbd1170da24acfabcf286e7497a494
Nicolás de Prat Gay <nicoprat@hotmail.com>
2022-06-21 15:02:05 -0300

    Ejercicios terminados

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -147,6 +147,22 @@ function fizzBuzz(numero) {
 }
 
 function operadoresLogicos(num1, num2, num3) {
+  if (num1 < 0 || num2 < 0 || num3 < 0) {
+    return 'Hay negativos'
+  }
+  else if (num1 === 0 || num2 === 0 || num3 === 0 ) {
+    return 'Error'
+  }
+  else if (num1 > num2 && num1 > num3 && num1 > 0) {
+    return 'Número 1 es mayor y positivo'
+  }
+  else if (num3 > num1 && num3 > num2) {
+    return num3 + 1
+  }
+  else {
+    return false
+  }
+ 
   //La función recibe tres números distintos. 
   //Si num1 es mayor a num2 y a num3 y además es positivo, retornar ---> "Número 1 es mayor y positivo"
   //Si alguno de los tres números es negativo, retornar ---> "Hay negativos"
@@ -156,6 +172,18 @@ function operadoresLogicos(num1, num2, num3) {
 }
 
 function esPrimo(numero) {
+  if (numero < 2) {
+    return false
+  }
+  if (numero === 2) {
+    return true
+  }
+  for(var i = 2; i < numero; i++) {
+    if (numero % i === 0) {
+      return false
+    }
+  }
+  return true
   // Devuelve "true" si "numero" es primo
   // De lo contrario devuelve "falso"
   // Pista: un número primo solo es divisible por sí mismo y por 1
@@ -164,6 +192,10 @@ function esPrimo(numero) {
 }
 
 function esVerdadero(valor){
+  if (valor === true) {
+    return 'Soy verdadero'
+  }
+  return 'Soy falso'
   //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
   //si su valor es true y “Soy falso” si su valor es false.
   //Escribe tu código aquí
@@ -171,6 +203,11 @@ function esVerdadero(valor){
 }
 
 function tablaDelSeis(){
+  let tabla = []
+  for (let i = 0; i < 11; i++){
+    tabla.push(6 * i)
+  }
+  return tabla
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí   
@@ -178,12 +215,24 @@ function tablaDelSeis(){
 }
 
 function tieneTresDigitos(numero){
+  if (numero > 99 && numero < 1000){
+    return true
+  }
+  return false
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
   
 }
 
 function doWhile(numero) {
+  let a = numero
+  let i = 0
+  do {
+    i = i + 1
+    a = a + 5
+  
+  } while (i < 8)
+  return a
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
   //Usar el bucle do ... while.
